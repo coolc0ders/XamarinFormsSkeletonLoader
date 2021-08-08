f3eb350ac56fcfe6667362806fdd7d3c4c7489d6
Damien Doumer <damientohin@gmail.com>
2021-08-08 10:58:01 +0200

    added skeleton loader gradient animation

--- SkeletonLoaderDemo/SkeletonLoaderDemo/Views/CustomViews/SkeletonView.cs
+++ SkeletonLoaderDemo/SkeletonLoaderDemo/Views/CustomViews/SkeletonView.cs
@@ -32,7 +32,8 @@ namespace SkeletonLoaderDemo.Views.CustomViews
                     currentView.AbortAnimation(LoadingAnimationName);
                 else
                 {
-                    currentView.RunSkeletonColorAnimation();
+                    //currentView.RunSkeletonColorAnimation();
+                    currentView.RunSkeletonGradientAnimation();
                 }
             });
         public bool IsLoading
@@ -50,6 +51,38 @@ namespace SkeletonLoaderDemo.Views.CustomViews
                     StartColor.A + t * (EndColor.A - StartColor.A));
 
             this.Animate<Color>(LoadingAnimationName, transform, (color) => BackgroundColor = color, repeat: () => true, length: 2000);
+        }
+
+        void RunSkeletonGradientAnimation()
+        {
+            Func<double, (Color startColor, Color endColor)> transform = (t) =>
+            {
+                var startColor = Color.FromRgba(StartColor.R + t * (EndColor.R - StartColor.R),
+                    StartColor.G + t * (EndColor.G - StartColor.G),
+                    StartColor.B + t * (EndColor.B - StartColor.B),
+                    StartColor.A + t * (EndColor.A - StartColor.A));
+
+                var endColor = Color.FromRgba(StartColor.R - t * (EndColor.R - StartColor.R),
+                    StartColor.G + t * (StartColor.G - EndColor.G),
+                    StartColor.B + t * (StartColor.B - EndColor.B),
+                    StartColor.A + t * (StartColor.A - EndColor.A));
+
+                return (startColor, endColor);
+            };
+
+            this.Animate<(Color startColor, Color endColor)>(LoadingAnimationName, transform, (color) =>
+            {
+                Background = new LinearGradientBrush
+                {
+                    StartPoint = new Point(0, 0),
+                    EndPoint = new Point(1, 0),
+                    GradientStops = new GradientStopCollection
+                    {
+                        new GradientStop(color.startColor, 0.1f),
+                        new GradientStop(color.endColor, 0.0f)
+                    }
+                };
+            }, repeat: () => true, length: 1000);
         }
     }
 }
